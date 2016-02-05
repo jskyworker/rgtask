c70983442e7f768219b080cccb2d8b40d4a48c11
Yevhen Mykhailin <jskyworker@gmail.com>
2016-02-05 21:11:02 +0000

    Added presence and length vaildation for description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -57,4 +57,44 @@ describe "Creating todo lists" do
     expect(page).not_to have_content("Description of 2 list")
   end
 
+
+  it "displays an error in case todo list has no Description" do
+  	expect(TodoList.count).to eq(0)
+
+  	visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Some title"
+    fill_in "Description", with: ""
+    fill_in "Status", with: "open"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).not_to have_content("Some title")
+  end
+
+
+  it "displays an error in case todo list has a Description less than 3 characters" do
+  	expect(TodoList.count).to eq(0)
+
+  	visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Yo sdfkhdf"
+    fill_in "Description", with: "Yo"
+    fill_in "Status", with: "open"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).not_to have_content("Yo sdfkhdf")
+  end
+
 end
