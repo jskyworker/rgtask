95ea5c0b26c862ce1c6e8d223b90840a99ea0713
Yevhen Mykhailin <jskyworker@gmail.com>
2016-02-07 13:53:06 +0000

    Added test for editing items

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe TodoItemsController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
-    end
-  end
+#  describe "GET index" do
+#    it "returns http success" do
+#      get :index
+#      expect(response).to be_success
+#    end
+#  end
 
 end
--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -22,4 +22,28 @@ describe "Creating todo items" do
   	end
   end
 
+
+  it "displays an error with no content" do
+  	visit_todo_list(todo_list)
+  	click_link "New Todo Item"
+  	fill_in "Content", with: ""
+  	click_button "Save"
+  	within "div.flash" do
+      expect(page).to have_content("Failed to add todo list item.")
+  	end
+  	expect(page).to have_content("Content can't be blank")
+  end
+
+
+  it "displays an error with content less than 3 characters" do
+  	visit_todo_list(todo_list)
+  	click_link "New Todo Item"
+  	fill_in "Content", with: "XY"
+  	click_button "Save"
+  	within "div.flash" do
+      expect(page).to have_content("Failed to add todo list item.")
+  	end
+  	expect(page).to have_content("Content is too short")
+  end
+
 end
