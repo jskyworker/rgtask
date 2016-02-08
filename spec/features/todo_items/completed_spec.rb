c762266ac7c94bf1de2e147aff0bc75db8457665
Yevhen Mykhailin <jskyworker@gmail.com>
2016-02-08 20:19:40 +0000

    Some cleanups

--- spec/features/todo_items/completed_spec.rb
+++ spec/features/todo_items/completed_spec.rb
@@ -14,4 +14,22 @@ describe "Completing todo items" do
   	expect(todo_item.completed_at).to_not be_nil
   end
 
+  context "with completed items" do
+  	let!(:completed_todo_item) { todo_list.todo_items.create(content: "KDE", completed_at: 4.minutes.ago) }
+  	it "shows completed items as complete" do
+  	  visit_todo_list(todo_list)
+  	  within dom_id_for(completed_todo_item) do
+  	    expect(page).to have_content(completed_todo_item.completed_at)
+  	  end
+  	end
+
+  	it "doesn't shows 'Mark complete' after it was marked completed" do
+  	  visit_todo_list(todo_list)
+  	  within dom_id_for(completed_todo_item) do
+  	    expect(page).to_not have_content("Mark complete")
+  	  end
+  	end
+
+  end
+
 end
